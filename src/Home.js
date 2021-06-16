633fb99a1da28eaef0ced5c0c4703e7bdd56af0b
ilaylow <chuenleylow@gmail.com>
2021-06-16 18:41:02 +1000

    first commit

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Home extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <h1>Test test... This will be the portfolio website</h1>
+        )
+    }
+}
+
+export default Home;
